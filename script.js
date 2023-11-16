1801a3cb79033c6209d5eb194218b0df8ed46b95
Edmond <edmond.santiago7@gmail.com>
2023-11-16 23:58:07 +1100

    add ionicons script tag, add ionicons to header tag

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
 fetch('https://fakestoreapi.com/products/')
   .then(res=>res.json())
   .then(json=>console.log(json))
