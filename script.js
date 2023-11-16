c6cfc06a09ba54d4e42c832f95a5ce82fd7eb23c
Edmond <edmond.santiago7@gmail.com>
2023-11-16 23:32:57 +1100

    add metadata to head tags

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+fetch('https://fakestoreapi.com/products/')
+  .then(res=>res.json())
+  .then(json=>console.log(json))
+
